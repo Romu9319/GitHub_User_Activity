c9eb3b06810ffefa5223090cf5cd0dbd0143a4ee
unknown <danielerm1510@gmail.com>
2025-01-19 20:35:30 +0100

    command see activity built

--- cli.py
+++ cli.py
@@ -1,11 +1,20 @@
 import click
-import datetime
-import requests
+from github_api import get_events
 
 @click.group()
 def cli():
     pass
 
-@click.command()
+@cli.command()
 @click.argument('user', required=True)
+def github_activity(user):
+    events = get_events(user)
+    push_count = 0
 
+    for event in events:
+        if event['type'] == "PushEvent":
+            push_count += 1 
+    click.echo(f"Pushed {push_count} commits to {event['repo']['name']}")
+       
+if __name__ == "__main__":
+    cli()

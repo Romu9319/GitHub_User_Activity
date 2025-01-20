00b23ad89fee1ca3d213559f57e8917122f5d437
unknown <danielerm1510@gmail.com>
2025-01-20 21:59:30 +0100

    list activity by event finished

--- cli.py
+++ cli.py
@@ -9,15 +9,34 @@ def cli():
 @click.argument('user', required=True)
 def github_activity(user):
     events = get_events(user)
-    
     push_count = 0
+    previous_repository = None
 
     for event in events:
         if event['type'] == "PushEvent":
-            push_count += 1 
+            repository = event['repo']['name']
+            if repository == previous_repository:
+                push_count +=1
+            else:
+                if previous_repository:
+                    click.echo(f"Pushed {push_count} commits to {previous_repository}")
+                push_count = 1
+                previous_repository = repository
+    
+    if previous_repository:
+        click.echo(f"Pushed {push_count} commits to {previous_repository}")
+                
+                
+
+
+
+
+
+         
     #### NOTA CONTAR LAS EVENTOS DE TIPO IssueCommentEvent Y EL NOMBRE DE USUARIO Y REPO (TYPE['REPO']['NAME'])
-    issues = get_issues(user)
-    click.echo(f"Pushed {push_count} commits to {event['repo']['name']}")
-       
+    #issues = get_issues(user)
+    #click.echo(f"Pushed {push_count} commits to {event['repo']['name']}")
+    #click.echo(f"{event['type']}")
+
 if __name__ == "__main__":
     cli()

e9722317aae293ef4c1c64d83cedf280e1c9d4fd
unknown <danielerm1510@gmail.com>
2025-01-20 21:07:05 +0100

    events issues in progress

--- cli.py
+++ cli.py
@@ -1,5 +1,5 @@
 import click
-from github_api import get_events
+from github_api import get_events, get_issues
 
 @click.group()
 def cli():
@@ -9,11 +9,14 @@ def cli():
 @click.argument('user', required=True)
 def github_activity(user):
     events = get_events(user)
+    
     push_count = 0
 
     for event in events:
         if event['type'] == "PushEvent":
             push_count += 1 
+    #### NOTA CONTAR LAS EVENTOS DE TIPO IssueCommentEvent Y EL NOMBRE DE USUARIO Y REPO (TYPE['REPO']['NAME'])
+    issues = get_issues(user)
     click.echo(f"Pushed {push_count} commits to {event['repo']['name']}")
        
 if __name__ == "__main__":
--- github_api.py
+++ github_api.py
@@ -10,3 +10,15 @@ def get_events(user):
         return data
     else:
         raise Exception(f"Error {response.status_code}: {response.text}")
+    
+
+def get_issues(user, repo_name):
+    url_github = f"https://api.github.com/repos/{user}/{repo_name}/issues/events"
+    response = requests.get(url_github)
+
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        raise Exception(f"Error {response.status_code}: {response.text}")
